f9182816d11e4613b6d0d23599e326f42675d47c
NiceAesth <andrewcraft101@gmail.com>
2025-11-26 03:11:45 +0200

    chore: apply rider suggestions

--- BaciuAndreiLab7/AboutPage.xaml.cs
+++ BaciuAndreiLab7/AboutPage.xaml.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace BaciuAndreiLab7;
 
-public partial class AboutPage : ContentPage
+public partial class AboutPage
 {
     public AboutPage()
     {
--- BaciuAndreiLab7/AppShell.xaml.cs
+++ BaciuAndreiLab7/AppShell.xaml.cs
@@ -1,6 +1,6 @@
 ﻿namespace BaciuAndreiLab7;
 
-public partial class AppShell : Shell
+public partial class AppShell
 {
     public AppShell()
     {
