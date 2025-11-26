9ce556ea437686c311409024388c8423dce61a21
NiceAesth <andrewcraft101@gmail.com>
2025-11-26 02:13:45 +0200

    fix: runtime configs, update to .net9 for ios 26

---
+++ BaciuAndreiLab7/FontAwesome.cs
@@ -0,0 +1,34 @@
+namespace BaciuAndreiLab7;
+
+public static class FontAwesome
+{
+    public static class Solid
+    {
+        public const string ShoppingCart = "\uf07a";
+        public const string CircleInfo = "\uf05a";
+        public const string House = "\uf015";
+        public const string User = "\uf007";
+        public const string Gear = "\uf013";
+        public const string Bars = "\uf0c9";
+        public const string MagnifyingGlass = "\uf002";
+        public const string Plus = "\uf067";
+        public const string Trash = "\uf1f8";
+        public const string Edit = "\uf044";
+        public const string Check = "\uf00c";
+        public const string Times = "\uf00d";
+    }
+    
+    public static class Regular
+    {
+        public const string Heart = "\uf004";
+        public const string Star = "\uf005";
+        public const string User = "\uf007";
+    }
+    
+    public static class Brands
+    {
+        public const string Apple = "\uf179";
+        public const string Android = "\uf17b";
+        public const string Github = "\uf09b";
+    }
+}
